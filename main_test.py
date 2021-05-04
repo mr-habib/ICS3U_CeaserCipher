d3cf8ec4554708ee212f3922ffe9e9a120c40a8d
Mr. Habib <78511534+mr-habib@users.noreply.github.com>
2021-05-04 08:32:11 -0400

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,22 @@
+from main import *
+
+def test_ceaser_cipher_encode_normal():
+    assert ceaser_cipher_encode("abc", 1) == "bcd"
+    
+def test_ceaser_cipher_encode_normal_overflow():
+    assert ceaser_cipher_encode("xyza", 1) == "yzab"
+
+def test_ceaser_cipher_encode_words():
+    assert ceaser_cipher_encode("hello my name is mr. habib", 3) == "jgnnq oa pcog ku ot jcdkd"
+
+def test_ceaser_cipher_encode_ignore_symbols():
+    assert ceaser_cipher_encode("woah! there. are. a` few ~symbols here :j", 6) == "cugn! znkxk. gxk. g` lkc ~yeshury nkxk :p"
+
+def test_ceaser_cipher_encode_large_shift():
+    assert ceaser_cipher_encode("there is a large shift in power here", 30) == "xlivi mw e pevki wlmjx mr tsaiv livi"
+ 
+def test_ceaser_cipher_encode_empty_string():
+    assert ceaser_cipher_encode("", 1) == ""
+    
+def test_ceaser_cipher_encode_no_shift():
+    assert ceaser_cipher_encode("hello there", 0) == "hello there"
