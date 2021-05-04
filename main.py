ba81bdb0faf21f44d25c923e24b7aec353bda89f
Mr. Habib <78511534+mr-habib@users.noreply.github.com>
2021-05-04 08:15:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-# Firstname, L
+# Lastname, Firstname
 # mm/dd/yyyy
 # Description of your code
 
+def ceaser_cipher_encode(message, shift):
+    # Code goes here
+    pass
+
+def main():
+    message = input("Enter the message you wish to encode\n")
+    shift = int("Enter the shift amount: ")
+  
+    new_message = ceaser_cipher_encode(message, shift)
+    print(f"Decoded message: {new_message}")
+  
+  
+  
+if __name__ == '__main__':
+    main()
